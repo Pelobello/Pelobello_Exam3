e00fdedfe49338acd9d8fe56ce3ad31e18b40f10
USER <USER@DAZZLE>
2023-10-07 16:32:42 +0800

    Pelobello_Exam3_Final

--- src/pelobello_exam3/Pelobello_Exam3.java
+++ src/pelobello_exam3/Pelobello_Exam3.java
@@ -4,17 +4,113 @@
  */
 package pelobello_exam3;
 
-/**
- *
- * @author USER
- */
-public class Pelobello_Exam3 {
+import java.util.Scanner;
+import static pelobello_exam3.Pelobello_Exam3.PriorityQueueArrayLinkedList.print;
+import sun.print.PeekGraphics;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("hello");
+
+public class Pelobello_Exam3 { 
+  
+   public static class PriorityQueueArrayLinkedList {
+        private class PriorityQueueArray{
+            int priority;
+            int data;
+            PriorityQueueArray next;
+            
+        }
+         static int print=0;
+        private PriorityQueueArray front;
+         
+        private void insertion (int item,int priority) throws Exception{
+            PriorityQueueArray node = new PriorityQueueArray();
+            
+            node.data = item;
+            node.priority = priority;   
+            node.next = null;
+           
+            
+            if (front == null || priority <=front.priority) {
+                node.next = front;
+                front = node;
+            }else {
+                PriorityQueueArray temporary = front;
+                while (temporary.next != null &&temporary.next.priority<priority) {                    
+                    temporary = temporary.next;
+                }
+                node.next = temporary.next;
+                temporary.next = node;
+               print++;
+               
+            }
+            
+        }
+      
+        public int deletion()throws Exception{
+            if (front == null) {
+                throw new Exception("Empty");
+            }
+            int temporaty = front.data;
+            front = front.next;
+            
+            return temporaty;
+        }
+        public void display()throws Exception{
+            if (front == null) {
+                throw new Exception("Empty");
+            }
+            PriorityQueueArray temporary = front;
+            while (temporary !=null) {                
+                System.out.println(temporary.data+ "\t\t\t"+temporary.priority);
+                temporary = temporary.next;
+            }
+            System.out.println("\n");
+        }
+   }
+
+
+    private static PriorityQueueArrayLinkedList list = new PriorityQueueArrayLinkedList();
+    public static void main(String[] args) throws Exception{
+       
+      Scanner scan = new Scanner(System.in);
+  
+        while (true) {      
+            System.out.println("1. Enter data and priority");
+            System.out.println("Display the data");
+            System.out.print("enter here: ");
+            int data = scan.nextInt();
+            
+            switch (data) {
+                
+               
+                case 1:
+                    System.out.print("input data: ");
+                    int enter = scan.nextInt();
+                     System.out.print("input priority: ");
+                    int priority = scan.nextInt();
+                    list.insertion(enter, priority);
+                     
+                    if (print >4) {
+                        list.deletion();
+                        print--;
+                    }
+                    
+                    break;
+                case 2:
+                     list.display();
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+        }
+           
+       
+        
+      
+        
+        
+        
+        
     }
     
 }
+
